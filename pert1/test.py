602625fc34bd362bb6273a58115dd7758224d1e1
Bogiant Raskarowana <bogiant.raskarowana@bintang.com>
2021-11-29 13:13:33 +0700

    variabel

---
+++ pert1/test.py
@@ -0,0 +1,23 @@
+#inisiasi satu-satu
+a=1
+b=2
+c=3
+
+#inisiasi banyak
+d, e, f = 4, 5, 6
+
+#inisiasi bernilai sama
+g =h =i =7
+
+#untuk mencetak hasil
+print ('a:', a)
+print ('b:', b)
+print ('c:', c)
+print ('d:', d)
+print ('e:', e)
+print ('f:', f)
+print ('g:', g)
+print ('h:', h)
+print ('i:', i)
+
+
