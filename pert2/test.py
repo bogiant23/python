2980255fa55d871ae11a4061f5de6721625f57e5
Bogiant Raskarowana <bogiant.raskarowana@bintang.com>
2021-11-30 13:51:29 +0700

    akses data

--- pert2/test.py
+++ pert2/test.py
@@ -17,3 +17,18 @@ suhu_keluarga_ucup = ['ayah ucup', 19, 'ibu ucuo', 20]
 suhu_keluarga_boy = ['ayah boy', 20, 'anak boy', 18]
 #membuat list dalam list dan dicampur dengan data boolean
 suhu_keluarga = [suhu_keluarga_ucup, suhu_keluarga_boy, True]
+
+#Akses data list
+tinggi_badan = [162,177,182,150,166]
+
+#print posisi pertama
+print(tinggi_badan[0])
+
+#print posisi kedua dari belakang
+print(tinggi_badan[-2])
+
+#mengambil data dengan index 0,1,2,3
+print(tinggi_badan[:4])
+
+#mengambil data dengan index 2,3,4
+print(tinggi_badan[2:5])
