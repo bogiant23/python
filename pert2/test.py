f9e2832bb6c21f4b2557985018f649b334e52094
Bogiant Raskarowana <bogiant.raskarowana@bintang.com>
2021-11-30 13:45:09 +0700

    List Data

---
+++ pert2/test.py
@@ -0,0 +1,19 @@
+#membuat list
+#list berisi koleksi nilai/data
+#inisialisasi suhung anggota keluarga
+suhu1 = 21
+suhu2 = 20
+suhu3 = 22
+
+#simpan dalam list
+suhu_keluarga= [suhu1, suhu2, suhu3]
+
+print(suhu_keluarga[1])
+
+#list bisa berisi tipe data apapun dan tidak harus semua data berisi tipedata yang sama
+
+#membuat list
+suhu_keluarga_ucup = ['ayah ucup', 19, 'ibu ucuo', 20]
+suhu_keluarga_boy = ['ayah boy', 20, 'anak boy', 18]
+#membuat list dalam list dan dicampur dengan data boolean
+suhu_keluarga = [suhu_keluarga_ucup, suhu_keluarga_boy, True]
